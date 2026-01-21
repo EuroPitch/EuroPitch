4a10a627bbb16671a87b7aa8fe6b88c3a36147d2
Raj Aryan Upadhyaya <upadhyra@tcd.ie>
2026-01-21 18:56:54 +0530

    Fixed reg form

--- frontend/src/pages/Registration/Registration.jsx
+++ frontend/src/pages/Registration/Registration.jsx
@@ -2,16 +2,7 @@ export default function Registration() {
   return (
     <section className="container page">
       <h2>Register</h2>
-      <p>Fill out the form below to register your interest.</p>
-      <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLSdS3Q_w-UbjgEbL6q-MDArTAN1VVGplxWehprUC9Uw9obrIbQ/viewform?embedded=true"
-        width="100%"
-        height="1200"
-        style={{ border: "none" }}
-        title="EuroPitch Registration Form"
-      >
-        Loading…
-      </iframe>
+      <p><a href="https://forms.gle/DDvVPmCQdQvLEg6y6">Fill out the form through this link to register your interest!</a></p>
     </section>
   );
 }
