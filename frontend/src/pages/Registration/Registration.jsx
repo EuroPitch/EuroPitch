8377554e142c09c76250a51da2e7e922da3619a0
Raj Aryan Upadhyaya <upadhyra@tcd.ie>
2026-02-19 15:16:49 +0000

    Reg page gone

--- frontend/src/pages/Registration/Registration.jsx
+++
@@ -1,8 +0,0 @@
-export default function Registration() {
-  return (
-    <section className="container page">
-      <h2>Register</h2>
-      <p><a href="https://forms.gle/DDvVPmCQdQvLEg6y6">Fill out the form through this link to register your interest!</a></p>
-    </section>
-  );
-}
