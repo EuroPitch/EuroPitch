ecb3d003fec3455bd5903d5008f7c3349d26613d
Raj Aryan Upadhyaya <upadhyra@tcd.ie>
2026-02-19 15:15:57 +0000

    Better 404

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return (
     <div className="not-found">
-      <h1>404 - Page Not Found</h1>
-      <p>Oops! The page you're looking for doesn’t exist.</p>
+      <h1>404</h1>
+      <p>Oops — the page you're looking for doesn't exist.</p>
+      <Link to="/" className="btn primary">Back to Home</Link>
     </div>
   );
 };
